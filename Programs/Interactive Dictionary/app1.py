a4c26d9e4ff3e2dfb090b05c5834757c07dcda44
Anh Pham <anh65498@gmail.com>
2019-05-06 00:51:32 -0700

    fix grid() and pack() when displaying the definition on window. Need to test code in translate when user mispell.

--- Programs/Interactive Dictionary/app1.py
+++ Programs/Interactive Dictionary/app1.py
@@ -6,17 +6,13 @@ import tkinter.messagebox as tkmb   # for error window
 class Dictionary:
     def __init__(self):
         self.dict = json.load(open("data.json"))     # dictionary of key and value is a list
-        self.definition = ""       # a list of definition if found user input in our self.dict
-        self.message = ""          # message if not found user input in our self.dict
 
     def translate(self, word):
         '''
         Purpose: Search for word in dictionary. If user input is wrong, find the closest match to that input in our dictionary
         Params: w - word user input , dict - our dictionary
         Return: True if found, False if not found
-
         '''
-        print(word)
 
         if word.lower() in self.dict:
             return self.dict[word]
@@ -27,7 +23,7 @@ class Dictionary:
 
         # if user mispell
         elif len(get_close_matches(word, self.dict.keys())) > 0:
-            yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, self.dict.keys())[0]).lower()
+            yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, self.dict.keys())[0]).lower()
             if yn == "y":
                 return self.dict[get_close_matches(word, self.dict.keys())[0]]
             elif yn == "n":
@@ -44,20 +40,24 @@ class mainWin(tk.Tk):
         Purpose: Create and display the main window that let user enter a word to look up
                  and display the definition if found
         '''
+        self.word = ""
         super().__init__()              # run the constructor of tk. Pass nothing because this is main window
         self.minsize(550, 200)
-        self.geometry("+350+250")
+
+        self.geometry("+150+250")
         self.title("English Dictionary")
 
-        L = tk.Label(self, text="Enter a word you want to look up: ", font=("Arial", 18)).grid()
+        L = tk.Label(self, text="Enter a word you want to look up: ", font=("Arial", 18, "bold")).grid(sticky='w')
         # instance variables because we need to access them in another method of this class
         self.input_word = tk.StringVar()
         self.E = tk.Entry(self, textvariable=self.input_word)
-        self.E.grid(row=0, column=1)          # must be seperated from the self.E because AttributeError: 'NoneType' object has no attribute 'bind'
-        self.E.bind("<Return>", self.getDefinition)
+        self.E.grid(row=0, padx=300, sticky='w')          # must be seperated from the self.E because AttributeError: 'NoneType' object has no attribute 'bind'
+        self.E.bind("<Return>", self.getDefinition)     # python doesn't wait for callback to finish
 
-
-
+        self.L1 = tk.Label(self, text='', font=("Arial", 16, "bold"))       # for word
+        self.L1.grid(sticky='w', pady=10)
+        self.L2 = tk.Label(self, text='', font=("Arial", 14))       # for defintion
+        self.L2.grid(sticky='w', padx=30)
 
     def getDefinition(self, event):
         '''
@@ -66,31 +66,33 @@ class mainWin(tk.Tk):
                  Auto clear the input field after user press Enter
         Params, Ret: none
         '''
-
         word = str(self.input_word.get())
-        # print("Word in getDef:", word)
-        result = Dictionary().translate(word)
+        result = Dictionary().translate(word)   # return a list of definition
+        print(result, "\n\n")
 
-        L1 = tk.Label(self, text=word).grid()
+        # Overwrite word
+        self.L1.config(text=word.title())
+        # Overwrite definition
+        for i in range(len(result)):
+            result[i] = result[i].replace("e.g.", "for example: ")
+            result[i] = result[i].replace(". ", ".\n")
 
-        for definition in result:
-            tk.Label(self, text=definition).grid(sticky="we")
+            result[i] = result[i].replace(".\\n", ".\n")
+
+        definitions = '\n'.join(result)
+        print(definitions)
+
+        
+        self.L2.config(text=definitions, anchor="w", justify="left")
+        # self.L2.grid(sticky="w")
 
         self.E.delete(0, tk.END)        # clear out the entry widget
 
-def main():
 
+
+def main():
     app = mainWin()
     app.mainloop()
-    #
-    # while (user_choice.lower() != "q"):
-    #     word = input("Enter word: ")
-    #
-    #     output = list(translate(word,dict))
-    #
-    #     for item in output:
-    #         print(item)
-    #     choice = input("Do you want to quit or continue? Press enter to continue, 'q' to quit: ")
 
 
 if __name__ == "__main__":
